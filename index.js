8a4d673d43089296883067891f56a281df27d661
Ramachandran <134304263+Ram24102002@users.noreply.github.com>
2024-09-30 10:14:01 +0530

    push2
    push2

---
+++ index.js
@@ -0,0 +1,81 @@
+
+
+
+// suggestion btn
+
+const form = document.getElementById('form');
+const result = document.getElementById('result');
+
+form.addEventListener('submit', function(e) {
+  e.preventDefault();
+  const formData = new FormData(form);
+  const object = Object.fromEntries(formData);
+  const json = JSON.stringify(object);
+  result.innerHTML = "Please wait..."
+
+    fetch('https://api.web3forms.com/submit', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'Accept': 'application/json'
+            },
+            body: json
+        })
+        .then(async (response) => {
+            let json = await response.json();
+            if (response.status == 200) {
+                result.innerHTML = "Form submitted successfully";
+            } else {
+                console.log(response);
+                result.innerHTML = json.message;
+            }
+        })
+        .catch(error => {
+            console.log(error);
+            result.innerHTML = "Something went wrong!";
+        })
+        .then(function() {
+            form.reset();
+            setTimeout(() => {
+                result.style.display = "none";
+            }, 3000);
+        });
+});
+
+
+
+function callToast() {
+    const toastTrigger = document.getElementById('liveToastBtn')
+const toastLiveExample = document.getElementById('liveToast')
+
+if (toastTrigger) {
+  const toastBootstrap = bootstrap.Toast.getOrCreateInstance(toastLiveExample)
+  toastTrigger.addEventListener('click', () => {
+    toastBootstrap.show()
+  })
+}}
+
+
+
+
+
+
+// AI use policy alert
+
+
+function SecurityAlert(){
+    
+    alert('Amazon Third-Party Generative AI use Policy prohibits sharing confidential information unless you obtain required approvals.')
+    
+}
+
+
+
+
+
+
+
+
+
+
+
