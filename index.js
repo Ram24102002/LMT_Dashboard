b463b5b917d768b1c36dba8561da21b706fbab3f
Ramachandran <134304263+Ram24102002@users.noreply.github.com>
2024-11-26 23:13:27 +0530

    QR
    Added QR in Offcanvas

--- index.js
+++ index.js
@@ -1,19 +1,31 @@
 
-
-
-
 function callToast() {
-    const toastTrigger = document.getElementById('liveToastBtn')
+  const toastTrigger = document.getElementById('liveToastBtn')
 const toastLiveExample = document.getElementById('liveToast')
 
 if (toastTrigger) {
-  const toastBootstrap = bootstrap.Toast.getOrCreateInstance(toastLiveExample)
-  toastTrigger.addEventListener('click', () => {
-    toastBootstrap.show()
-  })
+const toastBootstrap = bootstrap.Toast.getOrCreateInstance(toastLiveExample)
+toastTrigger.addEventListener('click', () => {
+  toastBootstrap.show()
+})
 }}
 
 
+
+
+function sendEmail(){
+  sendEmail.send({
+    Host : "smtp.google.com",
+    username : "lucifersprimeacc@gmail.com",
+    password : "lucifer@password",
+    To : "ramachandran24102002@gmail.com",
+    From : document.getElementById("alieas").value,
+    subject : "New Button Request",
+    Body : document.getElementById("textarea")
+  }).then(
+    message => alert(message)
+  );
+}
 
 
 
@@ -30,6 +42,22 @@ function SecurityAlert(){
 
 
 
+// QR GENERATOR
+
+document.getElementById('QR-input').addEventListener('input', function() {
+  document.getElementById('qrcode').innerHTML = ""; // Use the correct string ID
+  Hi(); // Trigger the Hi() function
+});
+
+function Hi() {
+  var inputElement = document.getElementById('QR-input');
+  var inputValue = inputElement.value;
+
+  // Ensure the 'qrcode' element exists before proceeding
+  if (document.getElementById('qrcode')) {
+      new QRCode(document.getElementById('qrcode'), inputValue); // Generate QR Code
+  }
+}
 
 
 
--- rough.js
+++ rough.js
@@ -1,22 +1,118 @@
-// const secA=document.getElementsByClassName('sectionA')[0];
-// const secB=document.getElementsByClassName('sectionB')[0];
-// const btn=document.getElementsByTagName('button')[0];
+document.getElementById('input').addEventListener('input', function() {
+    document.getElementById('qrcode').innerHTML = ""; // Use the correct string ID
+    Hi(); // Trigger the Hi() function
+});
 
-// secA.addEventListener('mouseenter', function() {
-//     btn.style.display='none';
-// });
+function Hi() {
+    var inputElement = document.getElementById('input');
+    var inputValue = inputElement.value;
+
+    // Ensure the 'qrcode' element exists before proceeding
+    if (document.getElementById('qrcode')) {
+        new QRCode(document.getElementById('qrcode'), inputValue); // Generate QR Code
+    }
+}
 
 
 
-    // const secA = document.querySelector('.sectionA');
-    // const btn = document.querySelector('button');
 
-    // if (secA && btn) {
-    //     secA.addEventListener('mouseenter', function() {
-    //         console.log('Mouse entered sectionA');
-    //         btn.remove(); // This should work if everything is set correctly
-    //     });
-    // } else {
-    //     console.error('Element not found');
-    // }
+// function Hi(){
+//     var inputElement= document.getElementById('input')
+//     var inputValue=inputElement.value;
+    
+//     new QRCode(qrcode, inputValue );
+// }
 
+
+// document.getElementById('input').addEventListener('input', function(){
+    
+    
+//         document.getElementById(qrcode).innerHTML = "";
+//         Hi();
+    
+// })
+
+
+
+
+
+
+
+// var nestedArr=[[1,2,3],[4,5,6],[7,8,9]]
+
+// for(var i=0;i<nestedArr.length; i++)
+// {
+//     for(var j=0;j<arr[i].length; j++)
+//         {
+
+//             product*=arr[i][j];
+//             console.log(product);
+//         }
+// }
+
+
+
+// myArray =[1,2,3,4,5];
+// saveArr=0;
+// for(var i=0;i<myArray.length;i++)
+// {
+//     saveArr+=myArray[i];
+    
+// };
+
+
+// console.log(saveArr);
+
+
+
+
+
+// myArray =[];
+
+// for(var i=1;i<=5;i+=2)
+// {
+//     myArray.push(i);
+// };
+
+// console.log(myArray);
+
+
+
+
+// function lookup(val){
+//     var result ="";
+   
+// var object = {
+//     "name":"ram",
+//     "age":18
+// };
+
+// result = object[val];
+// return result;
+
+// // var b= object.age;
+// // console.log(b);
+
+// // object['color']="green";
+
+// // console.log(object.color);
+
+
+
+// }
+// console.log(lookup("name"));
+
+
+    
+
+// var input = document.getElementById('input');
+
+// var a = Number(input.value);
+// var b;
+// var c;
+// function add(b){
+//     c=a+b
+//     console.log(c)
+//     return c;
+// }
+// add(5);
